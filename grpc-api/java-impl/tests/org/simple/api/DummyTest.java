6b3fae19e7d625f2c268be295a7d763fe966f0a8
aldhinn <aldespinas@gmail.com>
2022-01-25 11:23:34 -0700

    set up a project module for the java implementation of the grpc api schema

---
+++ grpc-api/java-impl/tests/org/simple/api/DummyTest.java
@@ -0,0 +1,13 @@
+package org.simple.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// TODO: Delete later when real tests are written.
+public class DummyTest {
+    @Test
+    public void testMethod() {
+        assertTrue(true);
+    }
+}
